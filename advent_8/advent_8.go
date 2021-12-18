2cd696a69bd72d8bdeb4c6be2fd1c4b94fe6a96e
Aleksey V. Mikhaylov <gmikhalytch@gmail.com>
2021-12-18 16:30:02 +0300

    [advent_8] creating variants w/ test

--- advent_8/advent_8.go
+++ advent_8/advent_8.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"math"
 	"sort"
@@ -32,6 +33,148 @@ func lexicographicallyMinimalString(variants []string) string {
 	}
 	sort.Strings(shortStrings)
 	return shortStrings[0]
+}
+
+func asRunes(in string) []rune {
+	return bytes.Runes([]byte(in))
+}
+func addPermutations(current [][]rune, variants ...[]rune) [][]rune {
+	result := make([][]rune, 0, len(current))
+	if len(current) == 0 {
+		current = append(current, make([]rune, 0))
+	}
+	for _, c := range current {
+		for _, v := range variants {
+			n := make([]rune, len(c))
+			copy(n, c)
+			n = append(n, v...)
+			result = append(result, n)
+		}
+	}
+	return result
+}
+
+func createCodeVariants(in string) []string {
+	result := make([][]rune, 0)
+	inLen := len(in)
+	for runeIdx := 0; runeIdx < inLen; {
+		r := in[runeIdx]
+		r2b := false
+		if runeIdx+1 < inLen && in[runeIdx+1] == r {
+			r2b = true
+		}
+		r3b := false
+		if r2b && runeIdx+2 < inLen && in[runeIdx+2] == r {
+			r3b = true
+		}
+		r4b := false
+		if r3b && runeIdx+3 < inLen && in[runeIdx+3] == r {
+			r4b = true
+		}
+		switch r {
+		case '2': // 3
+			if r3b {
+				result = addPermutations(result, asRunes("AAA"), asRunes("AB"), asRunes("BA"), asRunes("C"))
+				runeIdx += 3
+			} else if r2b {
+				result = addPermutations(result, asRunes("AA"), asRunes("B"))
+				runeIdx += 2
+			} else {
+				result = addPermutations(result, asRunes("A"))
+				runeIdx += 1
+			}
+		case '3': // 3
+			if r3b {
+				result = addPermutations(result, asRunes("DDD"), asRunes("DE"), asRunes("ED"), asRunes("F"))
+				runeIdx += 3
+			} else if r2b {
+				result = addPermutations(result, asRunes("DD"), asRunes("E"))
+				runeIdx += 2
+			} else {
+				result = addPermutations(result, asRunes("D"))
+				runeIdx += 1
+			}
+		case '4': // 3
+			if r3b {
+				result = addPermutations(result, asRunes("GGG"), asRunes("GH"), asRunes("HG"), asRunes("I"))
+				runeIdx += 3
+			} else if r2b {
+				result = addPermutations(result, asRunes("GG"), asRunes("H"))
+				runeIdx += 2
+			} else {
+				result = addPermutations(result, asRunes("G"))
+				runeIdx += 1
+			}
+		case '5': // 3
+			if r3b {
+				result = addPermutations(result, asRunes("JJJ"), asRunes("JK"), asRunes("KJ"), asRunes("L"))
+				runeIdx += 3
+			} else if r2b {
+				result = addPermutations(result, asRunes("JJ"), asRunes("K"))
+				runeIdx += 2
+			} else {
+				result = addPermutations(result, asRunes("K"))
+				runeIdx += 1
+			}
+		case '6': // 3
+			if r3b {
+				result = addPermutations(result, asRunes("MMM"), asRunes("MN"), asRunes("NM"), asRunes("O"))
+				runeIdx += 3
+			} else if r2b {
+				result = addPermutations(result, asRunes("MM"), asRunes("N"))
+				runeIdx += 2
+			} else {
+				result = addPermutations(result, asRunes("M"))
+				runeIdx += 1
+			}
+		case '7': // 4
+			if r4b {
+				result = addPermutations(result, asRunes("PPPP"), asRunes("QPP"), asRunes("PQP"), asRunes("PPQ"), asRunes("QQ"), asRunes("PR"), asRunes("RP"), asRunes("S"))
+				runeIdx += 4
+			} else if r3b {
+				result = addPermutations(result, asRunes("PPP"), asRunes("PQ"), asRunes("QP"), asRunes("R"))
+				runeIdx += 3
+			} else if r2b {
+				result = addPermutations(result, asRunes("PP"), asRunes("Q"))
+				runeIdx += 2
+			} else {
+				result = addPermutations(result, asRunes("P"))
+				runeIdx += 1
+			}
+		case '8': // 3
+			if r3b {
+				result = addPermutations(result, asRunes("TTT"), asRunes("TU"), asRunes("UT"), asRunes("V"))
+				runeIdx += 3
+			} else if r2b {
+				result = addPermutations(result, asRunes("TT"), asRunes("U"))
+				runeIdx += 2
+			} else {
+				result = addPermutations(result, asRunes("T"))
+				runeIdx += 1
+			}
+		case '9': // 4
+			if r4b {
+				result = addPermutations(result, asRunes("WWWW"), asRunes("XWW"), asRunes("WWX"), asRunes("WXW"), asRunes("XX"), asRunes("WY"), asRunes("YW"), asRunes("Z"))
+				runeIdx += 4
+			} else if r3b {
+				result = addPermutations(result, asRunes("WWW"), asRunes("WX"), asRunes("XW"), asRunes("Y"))
+				runeIdx += 3
+			} else if r2b {
+				result = addPermutations(result, asRunes("WW"), asRunes("X"))
+				runeIdx += 2
+			} else {
+				result = addPermutations(result, asRunes("W"))
+				runeIdx += 1
+			}
+		default:
+			log.Fatalf("unexpected %v at index %d", r, runeIdx)
+		}
+	}
+	res := make([]string, 0, len(result))
+	for _, r := range result {
+		res = append(res, string(r))
+	}
+	return res
 }
 
 func calcAdvent8Result(inputFile advent8File) advent8Result {
--- advent_8/advent_8_test.go
+++ advent_8/advent_8_test.go
@@ -1,6 +1,43 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCalcAdvent8Result(t *testing.T) {
+	filename := "advent_8.sample"
+	want := "BE"
+	inputFile := readAdvent8File(filename)
+	gotResult := calcAdvent8Result(inputFile)
+
+	assertStrings(t, gotResult.value, want)
+}
+
+func TestCreateCodeVariants(t *testing.T) {
+	tests := []struct {
+		name     string
+		in       string
+		variants []string
+	}{
+		{"sample variant", "2233", []string{"AADD", "AAE", "BDD", "BE"}},
+		{"Kevin's variant", "222", []string{"AAA", "AB", "BA", "C"}},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			want := test.variants
+			got := createCodeVariants(test.in)
+
+			assertSlices(t, got, want)
+		})
+	}
+}
+
+func assertSlices(t *testing.T, got []string, want []string) {
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got %v, want %v", got, want)
+	}
+}
 
 func TestLexicographicallyMinimalString(t *testing.T) {
 	tests := []struct {
