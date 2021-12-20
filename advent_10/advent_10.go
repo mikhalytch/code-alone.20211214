3e76d1f65e6bb7e9183fcb73a3efb047e70265f2
Aleksey V. Mikhaylov <gmikhalytch@gmail.com>
2021-12-20 14:17:33 +0300

    [advent_10] readFile

--- advent_10/advent_10.go
+++ advent_10/advent_10.go
@@ -1,6 +1,12 @@
 package main
 
-import "log"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
 func main() {
 	filename := "advent_10.test.txt"
@@ -24,6 +30,34 @@ type advent10File struct {
 }
 
 func readAdvent10File(filename string) advent10File {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(fmt.Errorf("unable to open file %q: %w", filename, err))
+	}
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			log.Println(fmt.Errorf("unable to close file %q: %w", filename, err))
+		}
+	}()
+	scanner := bufio.NewScanner(file)
+
+	scanner.Split(bufio.ScanWords)
 	result := advent10File{}
+	for wordIndex := 0; scanner.Scan(); wordIndex++ {
+		text := scanner.Text()
+		number, err := strconv.Atoi(text)
+		if err != nil {
+			log.Fatal(fmt.Errorf("NaN %q at index %d: %w", text, wordIndex, err))
+		}
+		switch wordIndex {
+		case 0:
+			result.amount = number
+			result.sizes = make([]int, 0, number)
+		default:
+			result.sizes = append(result.sizes, number)
+		}
+	}
+
 	return result
 }
