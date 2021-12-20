9c3319a2497111215d2c5d491051cec06a050871
Aleksey V. Mikhaylov <gmikhalytch@gmail.com>
2021-12-20 15:34:49 +0300

    [advent_10] more testing

--- advent_10/advent_10_test.go
+++ advent_10/advent_10_test.go
@@ -11,12 +11,30 @@ const (
 )
 
 func TestCalcAdvent10Result(t *testing.T) {
-	t.Run("sample file", func(t *testing.T) {
-		gotResult := calcAdvent10Result(readAdvent10File(sampleFilename))
+	tests := []struct {
+		name     string
+		filename string
+		want     advent10Result
+	}{
+		{"received sample", sampleFilename, advent10Result{8, 2}},
+		{"changed received sample 1", "advent_10.1.sample", advent10Result{12, 2}},
+		{"changed received sample 2", "advent_10.2.sample", advent10Result{8, 2}},
+		{"changed received sample 3", "advent_10.3.sample", advent10Result{9, 1}},
+		{"changed received sample 4", "advent_10.4.sample", advent10Result{12, 4}},
+		{"changed received sample 5", "advent_10.5.sample", advent10Result{12, 2}},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := calcAdvent10Result(readAdvent10File(test.filename))
+			assertAdvent10Results(t, got, test.want)
+		})
+	}
+}
 
-		assertNumbers(t, gotResult.answer, 8)
-		assertNumbers(t, gotResult.firstSeenIdx, 2)
-	})
+func assertAdvent10Results(t *testing.T, got advent10Result, want advent10Result) {
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got %v, want %v", got, want)
+	}
 }
 
 func assertNumbers(t *testing.T, got int, want int) {
