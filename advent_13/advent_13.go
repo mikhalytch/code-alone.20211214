05a39475dfa488994852fa3f15cd82c9cfcfe672
Aleksey V. Mikhaylov <gmikhalytch@gmail.com>
2021-12-23 13:55:50 +0300

    [advent_13] readFile

--- advent_13/advent_13.go
+++ advent_13/advent_13.go
@@ -1,6 +1,13 @@
 package main
 
-import "log"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
 const realFilename = "advent_13.test.txt"
 
@@ -25,6 +32,34 @@ type advent13File struct {
 }
 
 func readAdvent13File(filename string) advent13File {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(fmt.Errorf("error opening file %q: %w", filename, err))
+	}
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			log.Println(fmt.Errorf("error closing file %q: %w", filename, err))
+		}
+	}()
+
+	lineScanner := bufio.NewScanner(file)
+	lineScanner.Split(bufio.ScanLines)
 	result := advent13File{}
+	for lineIdx := 0; lineScanner.Scan(); lineIdx++ {
+		lineText := lineScanner.Text()
+		switch lineIdx {
+		case 0:
+			wordScanner := bufio.NewScanner(strings.NewReader(lineText))
+			wordScanner.Split(bufio.ScanWords)
+			wordScanner.Scan()
+			result.amount, _ = strconv.Atoi(wordScanner.Text())
+			wordScanner.Scan()
+			result.ascNum, _ = strconv.Atoi(wordScanner.Text())
+			result.phones = make([]string, 0, result.amount)
+		default:
+			result.phones = append(result.phones, lineText)
+		}
+	}
 	return result
 }
