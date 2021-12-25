915162b912f2b80457c070a7af3bcafc297e9055
Aleksey V. Mikhaylov <gmikhalytch@gmail.com>
2021-12-25 20:30:55 +0300

    [advent_15] calc

--- advent_15/advent_15.go
+++ advent_15/advent_15.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"math/bits"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -15,6 +17,145 @@ func main() {
 	inputFile := readAdvent15File(realFilename)
 	result := calcAdvent15Result(inputFile)
 	log.Printf("Answer: %d\n", result.answer)
+}
+
+type advent15Result struct {
+	answer int
+}
+
+func calcAdvent15Result(inputFile advent15File) advent15Result {
+	mmAgg := newMinMaxAggregator()
+	for _, p := range inputFile.points {
+		mmAgg.add(p)
+	}
+
+	minXAgg := newMinPerAxisAggregator()
+	for x := mmAgg.minX; x <= mmAgg.maxX; x++ {
+		p := point{x, 0}
+		d := p.sumXDistanceTo(inputFile.points...)
+		minXAgg.add(x, d)
+	}
+	minYAgg := newMinPerAxisAggregator()
+	for y := mmAgg.minY; y <= mmAgg.maxY; y++ {
+		p := point{0, y}
+		d := p.sumYDistanceTo(inputFile.points...)
+		minYAgg.add(y, d)
+	}
+	sepPoint := point{minXAgg.minValue(), minYAgg.minValue()}
+	result := advent15Result{sepPoint.abs()}
+	return result
+}
+
+func newMinMaxAggregator() minMaxAggregator {
+	return minMaxAggregator{math.MaxInt, math.MinInt, math.MaxInt, math.MinInt}
+}
+
+type minMaxAggregator struct {
+	minX, maxX, minY, maxY int
+}
+
+func (mma *minMaxAggregator) add(p point) {
+	mma.minX = minInt(mma.minX, p.x)
+	mma.maxX = maxInt(mma.maxX, p.x)
+	mma.minY = minInt(mma.minY, p.y)
+	mma.maxY = maxInt(mma.maxY, p.y)
+}
+
+func sum(x ...uint64) uint64 {
+	r := uint64(0)
+	for _, s := range x {
+		r = sum0(r, s)
+	}
+	return r
+}
+func sum0(a, b uint64) uint64 {
+	if res, carry := bits.Add64(a, b, 0); carry != 0 {
+		log.Fatalln(fmt.Errorf("addition overflow"))
+	} else {
+		return res
+	}
+	//goland:noinspection GoUnreachableCode
+	panic("should've never get here")
+}
+
+func xDistance(p1, p2 point) uint64 {
+	return intAbs(p1.x - p2.x)
+}
+func yDistance(p1 point, p2 point) uint64 {
+	return intAbs(p1.y - p2.y)
+}
+
+type point struct {
+	x, y int
+}
+
+func (p point) xDistanceTo(a point) uint64 {
+	return xDistance(p, a)
+}
+func (p point) yDistanceTo(a point) uint64 {
+	return yDistance(p, a)
+}
+func (p point) sumXDistanceTo(ps ...point) uint64 {
+	result := uint64(0)
+	for _, a := range ps {
+		result = sum(result, p.xDistanceTo(a))
+	}
+	return result
+}
+func (p point) sumYDistanceTo(ps ...point) uint64 {
+	result := uint64(0)
+	for _, a := range ps {
+		result = sum(result, p.yDistanceTo(a))
+	}
+	return result
+}
+func (p point) sumDistanceTo(ps ...point) uint64 {
+	return sum(p.sumXDistanceTo(ps...), p.sumYDistanceTo(ps...))
+}
+func (p point) abs() int {
+	return p.y + p.x
+}
+
+func minInt(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+func maxInt(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func intAbs(i int) uint64 {
+	if i < 0 {
+		return uint64(-i)
+	}
+	return uint64(i)
+}
+
+func newMinPerAxisAggregator() minPerAxisAggregator {
+	return minPerAxisAggregator{minDistance: math.MaxUint64}
+}
+
+type minPerAxisAggregator struct {
+	minDistance      uint64
+	axisValuesForMin []int
+}
+
+func (aa *minPerAxisAggregator) add(axisValue int, d uint64) {
+	if d < aa.minDistance {
+		aa.minDistance = d
+		aa.axisValuesForMin = []int{axisValue}
+	} else if d == aa.minDistance {
+		aa.axisValuesForMin = append(aa.axisValuesForMin, axisValue)
+	}
+}
+func (aa minPerAxisAggregator) minValue() int {
+	values := aa.axisValuesForMin
+	sort.Ints(values)
+	return values[0]
 }
 
 func readAdvent15File(filename string) advent15File {
@@ -49,7 +190,7 @@ func readAdvent15File(filename string) advent15File {
 			if wordIdx%2 == 1 {
 				p = point{number, 0}
 			} else {
-				p.i = number
+				p.y = number
 				result.points = append(result.points, p)
 			}
 		}
@@ -58,55 +199,7 @@ func readAdvent15File(filename string) advent15File {
 	return result
 }
 
-func intAbs(i int) uint64 {
-	if i < 0 {
-		return uint64(-i)
-	}
-	return uint64(i)
-}
-func distance(p1, p2 point) uint64 {
-	return sum(intAbs(p1.i-p2.i), intAbs(p1.r-p2.r))
-}
-
-type point struct {
-	r /*x*/, i /*y*/ int
-}
-
-func (p point) distanceTo(a point) uint64 {
-	return distance(p, a)
-}
-func (p point) sumDistance(ps ...point) uint64 {
-	result := uint64(0)
-	for _, a := range ps {
-		result = sum(result, p.distanceTo(a))
-	}
-	return result
-}
-
-func (p *point) asComplex() complex128 {
-	return complex(float64(p.r), float64(p.i))
-}
-
 type advent15File struct {
 	pointsAmount int
 	points       []point
-}
-
-type advent15Result struct {
-	answer int
-}
-
-func calcAdvent15Result(inputFile advent15File) advent15Result {
-	result := advent15Result{}
-	return result
-}
-
-func sum(a, b uint64) uint64 {
-	if res, carry := bits.Add64(a, b, 0); carry != 0 {
-		log.Fatalln(fmt.Errorf("addition overflow"))
-	} else {
-		return res
-	}
-	//goland:noinspection GoUnreachableCode
-	panic("should've never get here")
 }
--- advent_15/advent_15_test.go
+++ advent_15/advent_15_test.go
@@ -22,29 +22,59 @@ func TestReadAdvent15File(t *testing.T) {
 }
 
 func TestCalcAdvent15Result(t *testing.T) {
-	t.Run("sample", func(t *testing.T) {
-		gotResult := calcAdvent15Result(readAdvent15File(sampleFilename))
-		assertInts(t, gotResult.answer, 3)
-	})
-}
-func TestDistanceTo(t *testing.T) {
 	tests := []struct {
 		name     string
-		point    point
-		distance uint64
+		filename string
+		want     int
 	}{
-		{"(1, 1)", point{1, 1}, 1},
-		{"(3, 3)", point{3, 3}, 3},
-		{"(1, 2)", point{1, 2}, 0},
-		{"(3, 2)", point{3, 2}, 2},
+		{"sample", sampleFilename, 3},
+		{"real file calculation (for refactoring)", realFilename, 95590},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			got := point{1, 2}.distanceTo(test.point)
-			want := test.distance
-			assertUints(t, got, want)
+			got := calcAdvent15Result(readAdvent15File(test.filename))
+			assertInts(t, got.answer, test.want)
 		})
 	}
+}
+func TestDistanceTo(t *testing.T) {
+	t.Run("point{1,2} to single point", func(t *testing.T) {
+		tests := []struct {
+			name     string
+			p        point
+			distance uint64
+		}{
+			{"(1, 1)", point{1, 1}, 1},
+			{"(3, 3)", point{3, 3}, 3},
+			{"(1, 2)", point{1, 2}, 0},
+			{"(3, 2)", point{3, 2}, 2},
+		}
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				got := point{1, 2}.sumDistanceTo(test.p)
+				want := test.distance
+				assertUints(t, got, want)
+			})
+		}
+	})
+	t.Run("single point to multiple points", func(t *testing.T) {
+		points := []point{{1, 1}, {3, 3}, {1, 2}, {3, 2}}
+		tests := []struct {
+			name     string
+			p        point
+			distance uint64
+		}{
+			{"(1, 2)", point{1, 2}, 6},
+			{"(2, 2)", point{2, 2}, 6},
+		}
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				got := test.p.sumDistanceTo(points...)
+				want := test.distance
+				assertUints(t, got, want)
+			})
+		}
+	})
 }
 
 func assertInts(t *testing.T, got int, want int) {
